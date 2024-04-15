ad2d6c7a016fe1768e764c6e307879b161ec2f24
farahBassoumi <bassoumiifarah@gmail.com>
2024-04-15 18:08:56 +0100

    adding the 3d model

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 // index.tsx or index.js
 import { BrowserRouter } from "react-router-dom";
-
+import React from "react";  
+import ComputersCanvas from "./computers";
 // import React from 'react';
 
 const App = () => {
   return (
     
     <BrowserRouter>
+    <div>
         <h1>hjjjjjjjk</h1>
+<ComputersCanvas/>
+        </div>
 
     </BrowserRouter>
   
