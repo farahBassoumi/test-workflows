3813c6071935d73334540755008c857df77188db
farahBassoumi <bassoumiifarah@gmail.com>
2024-04-15 18:10:41 +0100

    adding the 3d model 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 // index.tsx or index.js
 import { BrowserRouter } from "react-router-dom";
-import React from "react";  
 import ComputersCanvas from "./computers";
 // import React from 'react';
 
