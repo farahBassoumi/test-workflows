f43390104a4dfd153fe93b4cc42a320cfca97205
farahBassoumi <bassoumiifarah@gmail.com>
2024-04-17 22:03:49 +0100

    planet

--- src/computers.jsx
+++ src/computers.jsx
@@ -9,7 +9,7 @@ import { createRoot } from "react-dom/client";
 const Computers = ({ isMobile }) => {
   // const root=createRoot(container);
   //GLTFLoader.prototype.setPath('/test-workflows/public'); 
-  const computer = useGLTF("https://farahbassoumi.github.io/test-workflows/robot/scene.gltf");
+  const computer = useGLTF("https://farahbassoumi.github.io/test-workflows/planet/scene.gltf");
   return (
     <mesh>
       <hemisphereLight intensity={1} groundColor="black" />
